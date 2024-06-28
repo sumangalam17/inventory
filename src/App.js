7cc1ebdd312ef254d2ccd65453019043b0f7ed4a
sumangalam17 <65527787+sumangalam17@users.noreply.github.com>
2024-06-28 17:45:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Login } from './components/Login';
+
+function App() {
+  return (
+    <div>
+      <h1 className="App p-10 bg-blue border-black border-2 rounded">hello from react</h1>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
